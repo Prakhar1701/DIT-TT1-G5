5941181eee88b10fc7c1d9135ee30bf2c33cfcd2
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-02-05 19:47:52 +0530

    Solved microsoft_teams_assignments/due5feb2023/solution2/B.java

--- src/microsoft_teams_assignments/due5feb2023/solution2/B.java
+++ src/microsoft_teams_assignments/due5feb2023/solution2/B.java
@@ -1,4 +1,21 @@
 package microsoft_teams_assignments.due5feb2023.solution2;
 
 public class B {
+    public static void main(String[] args) {
+        B obj = new B();
+        int gcd = obj.gcd(5, 9);
+        System.out.println(gcd);
+    }
+
+    public int gcd(int A, int B) {
+        int min = Math.min(A, B);
+
+        for (int i = min; i > 0; i--) {
+            if (A % i == 0 && B % i == 0) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
 }
