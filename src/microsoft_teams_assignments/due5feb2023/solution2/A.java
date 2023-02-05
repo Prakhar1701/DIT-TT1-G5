58dfe57ce1b65755d718e1997251fc5ec885a905
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-02-05 18:22:04 +0530

    Solved microsoft_teams_assignments/due5feb2023/solution2/A.java

--- src/microsoft_teams_assignments/due5feb2023/solution2/A.java
+++ src/microsoft_teams_assignments/due5feb2023/solution2/A.java
@@ -1,4 +1,24 @@
 package microsoft_teams_assignments.due5feb2023.solution2;
 
 public class A {
+    public static void main(String[] args) {
+        int n = 121;
+        System.out.println(isPrime(n));
+    }
+    static String isPrime(int n) {
+
+        if(n<2){
+            return "No";
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            // Write your logic here
+            // Return "Yes" if n is prime, else return "No"
+            /*n is prime only if it is not divisible by any i.
+            We will not reach till n and 1 is already discarded*/
+            if(n%i==0){
+                return "No";
+            }
+        }
+        return "Yes";
+    }
 }
