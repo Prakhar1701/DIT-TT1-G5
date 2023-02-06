c891433dca346a57661bd73bee015a06b8eeb184
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-02-06 10:19:16 +0530

    Solved Optimized Approach-2 microsoft_teams_assignments/due5feb2023/solution2/B.java

--- src/microsoft_teams_assignments/due5feb2023/solution2/B.java
+++ src/microsoft_teams_assignments/due5feb2023/solution2/B.java
@@ -3,19 +3,32 @@ package microsoft_teams_assignments.due5feb2023.solution2;
 public class B {
     public static void main(String[] args) {
         B obj = new B();
-        int gcd = obj.gcd(5, 9);
+        int gcd = obj.gcd(8, 4);
         System.out.println(gcd);
     }
 
+    //Approach 2:
     public int gcd(int A, int B) {
-        int min = Math.min(A, B);
-
-        for (int i = min; i > 0; i--) {
-            if (A % i == 0 && B % i == 0) {
-                return i;
+        while (A != B) {
+            if (A > B) {
+                A = A - B;
+            } else {
+                B = B - A;
             }
         }
-
-        return 0;
+        return A;
     }
+
+    //Approach 1:
+//    public int gcd(int A, int B) {
+//        int min = Math.min(A, B);
+//
+//        for (int i = min; i > 0; i--) {
+//            if (A % i == 0 && B % i == 0) {
+//                return i;
+//            }
+//        }
+//
+//        return 0;
+//    }
 }
