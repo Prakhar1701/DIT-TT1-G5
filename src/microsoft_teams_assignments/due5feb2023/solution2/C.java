633e9905b7ed29cd1183ec0359dc0a1e68761c63
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-02-06 15:19:36 +0530

    Solved Optimized Approach-2 microsoft_teams_assignments/due5feb2023/solution2/C.java

--- src/microsoft_teams_assignments/due5feb2023/solution2/C.java
+++ src/microsoft_teams_assignments/due5feb2023/solution2/C.java
@@ -2,30 +2,30 @@ package microsoft_teams_assignments.due5feb2023.solution2;
 
 public class C {
     public static void main(String[] args) {
-        Long[] ans = lcmAndGcd(8L, 14L);
-        System.out.println("HCF: "+ans[0]+", GCD: "+ans[1]);
+        Long[] ans = lcmAndGcd(14L, 8L);
+        System.out.println("HCF: " + ans[0] + ", GCD: " + ans[1]);
     }
 
-
     static Long[] lcmAndGcd(Long A, Long B) {
-
         Long[] ans = new Long[2];
 
         //LCM
-        for (long i = Math.max(A, B); i <= A * B; i++) {
-            if (i % A == 0 && i % B == 0) {
-                ans[0] = i;
-                break;
-            }
+        long a = A;
+        long b = B;
+        while (a % b != 0) {
+            a = a + A;
         }
-        
+        ans[0] = a;
+
         //GCD
-        while (A % B != 0) {
-            long r = A % B;
-            A = B;
-            B = r;
+        a = A;
+        b = B;
+        while (a % b != 0) {
+            long r = a % b;
+            a = b;
+            b = r;
         }
-        ans[1] = B;
+        ans[1] = b;
 
         return ans;
     }
