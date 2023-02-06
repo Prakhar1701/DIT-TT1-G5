ba6c426c9ada2783014461bf5d2118d23417844d
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-02-06 15:36:48 +0530

    Solved Optimized Approach-3 microsoft_teams_assignments/due5feb2023/solution2/C.java
    Approach: HCF Using GCD

--- src/microsoft_teams_assignments/due5feb2023/solution2/C.java
+++ src/microsoft_teams_assignments/due5feb2023/solution2/C.java
@@ -2,30 +2,26 @@ package microsoft_teams_assignments.due5feb2023.solution2;
 
 public class C {
     public static void main(String[] args) {
-        Long[] ans = lcmAndGcd(14L, 8L);
+        Long[] ans = lcmAndGcd(15L, 3L);
         System.out.println("HCF: " + ans[0] + ", GCD: " + ans[1]);
     }
 
     static Long[] lcmAndGcd(Long A, Long B) {
         Long[] ans = new Long[2];
 
-        //LCM
         long a = A;
         long b = B;
-        while (a % b != 0) {
-            a = a + A;
-        }
-        ans[0] = a;
 
         //GCD
-        a = A;
-        b = B;
         while (a % b != 0) {
             long r = a % b;
             a = b;
             b = r;
         }
         ans[1] = b;
+
+        //LCM
+        ans[0] = A*B/ans[1];
 
         return ans;
     }
